8c47881f45cc918e176256d8bc0c7ec3eb650caa
leenattal <94069754+leenattal@users.noreply.github.com>
2022-03-28 12:21:56 +0200

    any coment

--- src/app/Services/report.service.ts
+++ src/app/Services/report.service.ts
@@ -10,12 +10,24 @@ import { ChartData, ChartOptions } from 'chart.js';
 export class ReportService {
 
   report: any =[];
+  report1: any =[];
+  report2: any =[];
+  report3: any =[];
   staitstics:any =[];
   newUsers:any;
   salaries:any = [];
   totalOrders:any;
+  totalOrders1:any;
+  totalOrders2:any;
+  totalOrders3:any;
   totalFines:any;
+  totalFines1:any;
+  totalFines2:any;
+  totalFines3:any;
   totalSales:any;
+  totalSales1:any;
+  totalSales2:any;
+  totalSales3:any;
   totlatSalary:any;
 
   salesData!: ChartData<'bar'> ;
@@ -106,5 +118,78 @@ export class ReportService {
       this.toastr.error(err.message,err.status);
     });
   }
+
+  getDailyBook(){
+    this.spinner.show();
+    this.http.get("https://localhost:44346/api/report/BookDailyReport").subscribe((res:any)=>{
+      this.spinner.hide();
+      this.report1= res;
+      this.totalOrders1=res.map((a:any) => a.orders).reduce(function(a:any, b:any){return a + b;})
+      this.totalSales1=res.map((a:any) => a.sales).reduce(function(a:any, b:any){return a + b;})
+      this.totalFines1=res.map((a:any) => a.fines).reduce(function(a:any, b:any){return a + b;})
+      this.salesData= {
+        labels: res.map(function(a:any) {return a.period;}).reverse(),
+        datasets: [
+          { label: 'Orders', data: res.map(function(a:any) {return a.orders;}).reverse() },
+          { label: 'Sales', data: res.map(function(a:any) {return a.sales;}).reverse() },
+          { label: 'Fines', data: res.map(function(a:any) {return a.fines;}).reverse() },
+          { label: 'Book', data: res.map(function(a:any) {return a.bookname;}).reverse() },
+          
+        ],
+      };
+    },err=>{
+      this.spinner.hide()
+      this.toastr.error(err.message,err.status);
+    })
+  }   
+
+
+  getMonthlyBook(){
+    this.spinner.show();
+    this.http.get("https://localhost:44346/api/report/BookMonthlyReport").subscribe((res:any)=>{
+      this.spinner.hide();
+      this.report2= res;
+      this.totalOrders2=res.map((a:any) => a.orders).reduce(function(a:any, b:any){return a + b;})
+      this.totalSales2=res.map((a:any) => a.sales).reduce(function(a:any, b:any){return a + b;})
+      this.totalFines2=res.map((a:any) => a.fines).reduce(function(a:any, b:any){return a + b;})
+      this.salesData= {
+        labels: res.map(function(a:any) {return a.period;}).reverse(),
+        datasets: [
+          { label: 'Orders', data: res.map(function(a:any) {return a.orders;}).reverse() },
+          { label: 'Sales', data: res.map(function(a:any) {return a.sales;}).reverse() },
+          { label: 'Fines', data: res.map(function(a:any) {return a.fines;}).reverse() },
+          { label: 'Book', data: res.map(function(a:any) {return a.bookname;}).reverse() },
+          
+        ],
+      };
+    },err=>{
+      this.spinner.hide()
+      this.toastr.error(err.message,err.status);
+    })
+  }   
+
+  getAnnualBook(){
+    this.spinner.show();
+    this.http.get("https://localhost:44346/api/report/BookAnnualReport").subscribe((res:any)=>{
+      this.spinner.hide();
+      this.report3= res;
+      this.totalOrders3=res.map((a:any) => a.orders).reduce(function(a:any, b:any){return a + b;})
+      this.totalSales3=res.map((a:any) => a.sales).reduce(function(a:any, b:any){return a + b;})
+      this.totalFines3=res.map((a:any) => a.fines).reduce(function(a:any, b:any){return a + b;})
+      this.salesData= {
+        labels: res.map(function(a:any) {return a.period;}).reverse(),
+        datasets: [
+          { label: 'Orders', data: res.map(function(a:any) {return a.orders;}).reverse() },
+          { label: 'Sales', data: res.map(function(a:any) {return a.sales;}).reverse() },
+          { label: 'Fines', data: res.map(function(a:any) {return a.fines;}).reverse() },
+          { label: 'Book', data: res.map(function(a:any) {return a.bookname;}).reverse() },
+          
+        ],
+      };
+    },err=>{
+      this.spinner.hide()
+      this.toastr.error(err.message,err.status);
+    })
+  }   
 
 }
--- src/app/accountant/dashboardacc/dashboardacc.component.ts
+++ src/app/accountant/dashboardacc/dashboardacc.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ReportService } from 'src/app/Services/report.service';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-dashboardacc',
@@ -7,9 +11,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardaccComponent implements OnInit {
 
-  constructor() { }
+  constructor(public reportService: ReportService) { }
 
   ngOnInit(): void {
+    this.reportService.getMonthlyReports();
+    this.reportService. getDailyBook();
+    this.reportService.getMonthlyBook();
+    this.reportService.getAnnualBook();
   }
+
+  changePeriod(period: any) {
+
+    if (period.target.value === 'monthly') {
+      this.reportService.getMonthlyReports();
+    }
+    else {
+      this.reportService.getAnnualReports();
+    }
+
+  }
+
+  downloadAsDailyPdf() {
+    const doc = new jsPDF();
+    autoTable(doc, { html: '#table' });
+    doc.save('DailyBook-report.pdf');
+  }
+  exportexcelDaily() {
+    const fileName = 'DailyBook-report.xlsx';
+    let element = document.getElementById('table');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+    var wscols = [
+      {wch:15},
+      {wch:7},
+      {wch:12},
+      {wch:12},
+      {wch:12}
+
+  ];
+
+  ws['!cols'] = wscols;
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    XLSX.writeFile(wb, fileName);
+  }
+
+  downloadAsMonthlyPdf() {
+    const doc = new jsPDF();
+    autoTable(doc, { html: '#table2' });
+    doc.save('MonthlyBook-report.pdf');
+  }
+
+  exportexcelMonthly() {
+    const fileName = 'Monthly-report.xlsx';
+    let element = document.getElementById('table2');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+    var wscols = [
+      {wch:15},
+      {wch:7},
+      {wch:12},
+      {wch:12},
+      {wch:12}
+
+  ];
+
+  ws['!cols'] = wscols;
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    XLSX.writeFile(wb, fileName);
+  }
+
+  downloadAsAnnoulPdf() {
+    const doc = new jsPDF();
+    autoTable(doc, { html: '#table3' });
+    doc.save('AnnoulBook-report.pdf');
+  }
+
+  exportexcelAnnoul() {
+    const fileName = 'Annoul-report.xlsx';
+    let element = document.getElementById('table3');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    XLSX.writeFile(wb, fileName);
+  }
+
+
+
+
+
 
 }
